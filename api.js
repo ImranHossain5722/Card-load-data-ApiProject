51765c08d89b9a162a1cf5bc0ad9e06e7476f652
ImranHossain5722 <imd572258@gmail.com>
2022-02-27 20:19:15 +0600

    working error handel done

--- api.js
+++ api.js
@@ -1,1 +1,30 @@
- fetch ('https://deckofcardsapi.com/api/deck/new/draw/?count=2')
+ const searchButton =()=>{
+ const input = document.getElementById('input')
+ const error = document.getElementById('error')
+ const inputValue = input.value ;
+    //error handel
+ if (isNaN (inputValue) || inputValue =="" ){
+    // alert("please put a number" )
+    error.innerText =" * please give a number"
+    input.value ="";
+ }else if (inputValue <= 0){
+
+    error.innerText = " * please give a Positive number"
+    input.value ="";
+ }else{
+    fetch (`https://deckofcardsapi.com/api/deck/new/draw/?count=${inputValue}`)
+    .then(res => res.json())
+    .then(dataLoad => cardDisplay(dataLoad))
+    input.value =""   
+}
+
+
+ }
+
+ // display function
+const cardDisplay = cardDataLoad =>{
+console.log(cardDataLoad);
+
+
+ }
+
